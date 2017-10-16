787207b26d9b9b42b08e7ac9e324920e5bfe53bd
tboonlpfrg <timoboon2@gmail.com>
2017-10-16 12:00:49 +0200

    fixed temprature bug (put back to 140 degrees)

--- Configuration.h
+++ Configuration.h
@@ -12,7 +12,7 @@
   #define STRING_CONFIG_H_AUTHOR "Leapfrog 3D Printers" //Who made the changes.
 
   #define LEAPFROG_LMC_VERSION "LMC v2"
-  #define LEAPFROG_FIRMWARE_VERSION "2.8"
+  #define LEAPFROG_FIRMWARE_VERSION "2.8.1"
 
   #define LEAPFROG_MODEL "BoltPro"
 
@@ -68,7 +68,7 @@
   // TODO: variable MAXTEMP based on high and low temp hotends?
   #define HEATER_LOW_TEMP_MAX 275
   #define HEATER_HIGH_TEMP_MAX 360
-  #define BED_MAXTEMP 90
+  #define BED_MAXTEMP 140
 
   #define USE_BED_PWM
 
