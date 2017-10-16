04ffcfc9a98ba3e06a163f921ddab89f893fb60f
tboonlpfrg <timoboon2@gmail.com>
2017-10-16 12:02:44 +0200

    Fixed temprature error (put back to 140 degrees for bed)

--- Configuration.h
+++ Configuration.h
@@ -12,7 +12,7 @@
   #define STRING_CONFIG_H_AUTHOR "Leapfrog 3D Printers" //Who made the changes.
 
   #define LEAPFROG_LMC_VERSION "LMC v2"
-  #define LEAPFROG_FIRMWARE_VERSION "2.8"
+  #define LEAPFROG_FIRMWARE_VERSION "2.8.1"
 
   #define LEAPFROG_MODEL "Bolt"
 
